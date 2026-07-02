42e7c887432c67ee2001109050d1597f84731ce0
misbahuddinemran2 <misbahuddinemran2@gmail.com>
2026-07-02 22:54:18 +0600

    Update ExamPlatformApplication.java

--- src/main/java/com/examplatform/ExamPlatformApplication.java
+++ src/main/java/com/examplatform/ExamPlatformApplication.java
@@ -2,11 +2,11 @@ package com.examplatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 public class ExamPlatformApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ExamPlatformApplication.class, args);
     }
