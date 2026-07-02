dafae588531f65c7305582bcab00aa1f8548f47a
misbahuddinemran2 <misbahuddinemran2@gmail.com>
2026-07-03 02:25:33 +0600

    Create LiveExamSummaryResponse.java

---
+++ src/main/java/com/examplatform/modules/liveexam/dto/LiveExamSummaryResponse.java
@@ -0,0 +1,37 @@
+
+package com.examplatform.modules.liveexam.dto;
+
+import lombok.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LiveExamSummaryResponse {
+    private String id;
+    private String name;
+    private String examCode;
+    private String examType;
+    private String description;
+
+    private List<String> subjectNames;
+    private List<String> chapterNames;
+    private List<String> topicNames;
+
+    private int totalQuestions;
+    private BigDecimal totalMarks;
+    private BigDecimal passMarks;
+    private int durationMinutes;
+
+    private LocalDate examDate;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    private List<String> targetLevels;
+    private boolean isPremiumOnly;
+}
