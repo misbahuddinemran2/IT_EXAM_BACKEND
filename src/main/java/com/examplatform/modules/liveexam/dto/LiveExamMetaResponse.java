b878c88138127366231b8c6176ccc2d50bb8052b
misbahuddinemran2 <misbahuddinemran2@gmail.com>
2026-07-03 13:48:05 +0600

    Create LiveExamMetaResponse.java

---
+++ src/main/java/com/examplatform/modules/liveexam/dto/LiveExamMetaResponse.java
@@ -0,0 +1,13 @@
+package com.examplatform.modules.liveexam.dto;
+
+import lombok.*;
+import java.util.List;
+
+@Getter @Setter @Builder @NoArgsConstructor @AllArgsConstructor
+public class LiveExamMetaResponse {
+    private String examId;
+    private String examName;
+    private List<String> subjectNames;
+    private List<String> chapterNames;
+    private List<String> topicNames;
+}
