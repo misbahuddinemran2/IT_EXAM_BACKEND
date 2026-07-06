b9df7a52dabdbe19fa130a103a17363b5005bd9a
misbahuddinemran2 <misbahuddinemran2@gmail.com>
2026-07-06 14:24:07 +0600

    Add files via upload

---
+++ src/main/java/com/examplatform/common/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.examplatform.common.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
