2e7b46f64561837ea5d9950ad80a17f6a83e7fc2
misbahuddinemran2 <misbahuddinemran2@gmail.com>
2026-07-07 20:09:17 +0600

    Create LeaderboardSettingsUpdateRequest.java

---
+++ src/main/java/com/examplatform/modules/leaderboard/dto/LeaderboardSettingsUpdateRequest.java
@@ -0,0 +1,17 @@
+package com.examplatform.modules.leaderboard.dto;
+
+import com.examplatform.modules.leaderboard.entity.LeaderboardSettings;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LeaderboardSettingsUpdateRequest {
+    private int overallMinExamsRequired;
+    private LeaderboardSettings.ThresholdType monthlyThresholdType;
+    private int monthlyMinExamsRequired;
+    private int monthlyAllowedMissedExams;
+    private boolean levelWiseSeparate;
+    private boolean isEnabled;
+}
