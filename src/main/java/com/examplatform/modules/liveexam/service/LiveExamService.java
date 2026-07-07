6744681791c248abfc24190364cdb85bf4938cb1
misbahuddinemran2 <misbahuddinemran2@gmail.com>
2026-07-07 20:48:08 +0600

    Update LiveExamService.java

--- src/main/java/com/examplatform/modules/liveexam/service/LiveExamService.java
+++ src/main/java/com/examplatform/modules/liveexam/service/LiveExamService.java
@@ -24,6 +24,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.jdbc.core.JdbcTemplate;
+import com.examplatform.modules.leaderboard.service.LeaderboardService;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
