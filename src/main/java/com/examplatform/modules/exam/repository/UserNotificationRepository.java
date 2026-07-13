a9263d31dafa6766772cd682f3e14910971356be
misbahuddinemran2 <misbahuddinemran2@gmail.com>
2026-07-13 22:57:15 +0600

    Update UserNotificationRepository.java

--- src/main/java/com/examplatform/modules/exam/repository/UserNotificationRepository.java
+++ src/main/java/com/examplatform/modules/exam/repository/UserNotificationRepository.java
@@ -3,6 +3,7 @@ package com.examplatform.modules.exam.repository;
 import com.examplatform.modules.exam.entity.UserNotification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.time.LocalDateTime;
 
 import java.util.List;
 
@@ -16,4 +17,11 @@ public interface UserNotificationRepository extends JpaRepository<UserNotificati
     int countByUserIdAndIsReadFalse(String userId);
 
     void deleteByUserIdAndIsReadTrue(String userId);
+    
+
+@Query("SELECT n FROM UserNotification n WHERE n.expiryDate IS NOT NULL AND n.expiryDate <= ?1")
+List<UserNotification> findExpiredNotifications(LocalDateTime now);
+
+@Query("SELECT n FROM UserNotification n ORDER BY n.sentAt DESC")
+List<UserNotification> findAllOrderBySentAtDesc();
 }
