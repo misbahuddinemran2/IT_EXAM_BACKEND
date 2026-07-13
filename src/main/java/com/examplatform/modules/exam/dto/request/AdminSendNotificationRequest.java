f9c1f29843413292d04931909c73299484ecb8c8
misbahuddinemran2 <misbahuddinemran2@gmail.com>
2026-07-13 16:35:10 +0600

    Create AdminSendNotificationRequest.java

---
+++ src/main/java/com/examplatform/modules/exam/dto/request/AdminSendNotificationRequest.java
@@ -0,0 +1,14 @@
+package com.examplatform.modules.exam.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AdminSendNotificationRequest {
+    private NotificationTargetType targetType;
+    private String educationLevel; // targetType = EDUCATION_LEVEL হলে দরকার (CLASS_9/NEW_CLASS_10/SSC/HSC_1ST_YEAR/HSC_2ND_YEAR)
+    private String examId;         // targetType = WRITTEN_EXAM বা MCQ_EXAM হলে দরকার
+    private String title;
+    private String body;
+}
