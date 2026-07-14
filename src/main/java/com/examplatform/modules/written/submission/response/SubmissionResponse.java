2e209b33cdb1a3f713259121c3340c35046c9ec8
misbahuddinemran2 <misbahuddinemran2@gmail.com>
2026-07-15 00:07:43 +0600

    Update SubmissionResponse.java

--- src/main/java/com/examplatform/modules/written/submission/response/SubmissionResponse.java
+++ src/main/java/com/examplatform/modules/written/submission/response/SubmissionResponse.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
