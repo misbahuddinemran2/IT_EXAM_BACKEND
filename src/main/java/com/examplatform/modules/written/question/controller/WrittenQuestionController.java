1871c28ad489ac398a3d88a2ef424395a357ead1
misbahuddinemran2 <misbahuddinemran2@gmail.com>
2026-07-14 23:09:01 +0600

    Update WrittenQuestionController.java

--- src/main/java/com/examplatform/modules/written/question/controller/WrittenQuestionController.java
+++ src/main/java/com/examplatform/modules/written/question/controller/WrittenQuestionController.java
@@ -1,10 +1,13 @@
 package com.examplatform.modules.written.question.controller;
 
 import com.examplatform.modules.written.question.response.QuestionStudentResponse;
+import com.examplatform.modules.written.question.response.QuestionWithAnswerResponse;
 import com.examplatform.modules.written.question.service.WrittenQuestionService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -18,5 +21,18 @@ public class WrittenQuestionController {
     @GetMapping("/exam/{examId}")
     public List<QuestionStudentResponse> getQuestionsForExam(@PathVariable String examId) {
         return questionService.getQuestionsForStudent(examId);
+    }
+
+    /**
+     * Questions + model/AI answers for a FINISHED exam only.
+     * Service throws IllegalStateException if the exam hasn't ended yet.
+     */
+    @GetMapping("/exam/{examId}/answers")
+    public List<QuestionWithAnswerResponse> getQuestionsWithAnswers(@PathVariable String examId) {
+        try {
+            return questionService.getQuestionsWithAnswers(examId);
+        } catch (IllegalStateException e) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage());
+        }
     }
 }
