7323d0499028910412c677dda8c5973974f70f29
misbahuddinemran2 <misbahuddinemran2@gmail.com>
2026-07-16 05:39:40 +0600

    Update WrittenEvaluationService.java

--- src/main/java/com/examplatform/modules/written/evaluation/service/WrittenEvaluationService.java
+++ src/main/java/com/examplatform/modules/written/evaluation/service/WrittenEvaluationService.java
@@ -1,6 +1,7 @@
 package com.examplatform.modules.written.evaluation.service;
 
 import com.examplatform.modules.written.evaluation.response.EvaluationResponse;
+import com.examplatform.modules.written.evaluation.response.WrittenLeaderboardEntryResponse;
 
 import java.util.List;
 
@@ -11,4 +12,6 @@ public interface WrittenEvaluationService {
     EvaluationResponse getEvaluationById(String evaluationId);
 
     List<EvaluationResponse> getEvaluationsForExam(String examId);
+
+    List<WrittenLeaderboardEntryResponse> getLeaderboard(String examId, String requestingUserId);
 }
