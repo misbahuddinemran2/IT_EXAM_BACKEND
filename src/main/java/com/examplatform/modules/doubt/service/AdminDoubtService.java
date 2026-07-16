9164fd4f83c7ed4202acbee2f968d3f208ea06d2
misbahuddinemran2 <misbahuddinemran2@gmail.com>
2026-07-16 22:34:27 +0600

    Create AdminDoubtService.java

---
+++ src/main/java/com/examplatform/modules/doubt/service/AdminDoubtService.java
@@ -0,0 +1,15 @@
+package com.examplatform.modules.doubt.service;
+
+import com.examplatform.modules.doubt.dto.AdminAnswerRequest;
+import com.examplatform.modules.doubt.dto.AiGenerateResponse;
+import com.examplatform.modules.doubt.dto.DoubtResponse;
+import com.examplatform.modules.doubt.dto.DoubtSummaryResponse;
+
+import java.util.List;
+
+public interface AdminDoubtService {
+    List<DoubtSummaryResponse> getByStatus(String status);
+    DoubtResponse acceptDoubt(String doubtId, String adminId);
+    AiGenerateResponse generateAiPreview(String doubtId);
+    DoubtResponse saveAnswer(String doubtId, String adminId, AdminAnswerRequest request);
+}
