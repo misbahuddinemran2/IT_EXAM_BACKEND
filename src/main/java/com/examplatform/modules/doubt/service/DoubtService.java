d96d41aa90c749d7ce9dfb19d8d750264a482a82
misbahuddinemran2 <misbahuddinemran2@gmail.com>
2026-07-16 22:33:13 +0600

    Create DoubtService.java

---
+++ src/main/java/com/examplatform/modules/doubt/service/DoubtService.java
@@ -0,0 +1,14 @@
+package com.examplatform.modules.doubt.service;
+
+import com.examplatform.modules.doubt.dto.*;
+
+import java.util.List;
+
+public interface DoubtService {
+    DoubtResponse createDoubt(String studentUserId, CreateDoubtRequest request);
+    String uploadQuestionFile(String doubtId, String studentUserId, org.springframework.web.multipart.MultipartFile file, boolean isPdf);
+    DoubtResponse updateDoubt(String doubtId, String studentUserId, UpdateDoubtRequest request);
+    List<DoubtSummaryResponse> getMyDoubts(String studentUserId);
+    List<DoubtSummaryResponse> getAnsweredDoubts(String chapterId, String subjectId);
+    DoubtResponse getDoubtDetail(String doubtId, String requesterUserId);
+}
