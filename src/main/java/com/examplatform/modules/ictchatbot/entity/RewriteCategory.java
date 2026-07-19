91b945fd6502fe5ba67050b68e07462d47c85a36
misbahuddinemran2 <misbahuddinemran2@gmail.com>
2026-07-19 11:11:09 +0600

    Create RewriteCategory.java

---
+++ src/main/java/com/examplatform/modules/ictchatbot/entity/RewriteCategory.java
@@ -0,0 +1,13 @@
+package com.examplatform.modules.ictchatbot.entity;
+
+public enum RewriteCategory {
+
+    SHORTEN,
+    EXPAND,
+    EXAM_FORMAT,
+    SIMPLIFY,
+    BULLET_POINTS,
+    IMPROVE,
+    WITH_EXAMPLE,
+    COMPARE_FORMAT
+}
