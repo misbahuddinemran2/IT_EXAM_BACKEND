41984acec4ea4d290a6ec7f003dd098e9863d2df
misbahuddinemran2 <misbahuddinemran2@gmail.com>
2026-07-19 11:16:12 +0600

    Create IctRewriteRequest.java

---
+++ src/main/java/com/examplatform/modules/ictchatbot/dto/IctRewriteRequest.java
@@ -0,0 +1,17 @@
+package com.examplatform.modules.ictchatbot.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class IctRewriteRequest {
+
+    private String originalAnswer;
+
+    private String instruction;
+}
