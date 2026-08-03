13ffe8b3caa6dbc3b60319011865becb37cfd562
misbahuddinemran2 <misbahuddinemran2@gmail.com>
2026-08-03 17:01:58 +0600

    Create EvaluationQuestionType.java

---
+++ src/main/java/com/examplatform/modules/evaluation/enums/EvaluationQuestionType.java
@@ -0,0 +1,19 @@
+package com.examplatform.modules.evaluation.enums;
+
+public enum EvaluationQuestionType {
+    DEFINITION,
+    FEATURES,
+    ADVANTAGE,
+    DISADVANTAGE,
+    APPLICATION,
+    EXAMPLE,
+    COMPARISON,
+    PROCESS,
+    IMPORTANCE,
+    CLASSIFICATION,
+    FORMULA_CALCULATION,
+    STRUCTURE,
+    FULL_FORM,
+    SYNTAX_CODE,
+    OTHER
+}
