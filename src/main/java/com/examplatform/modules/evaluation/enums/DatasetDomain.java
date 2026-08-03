2bf1ddb5a73cc489fc7fac07759c5aef9e59ac2b
misbahuddinemran2 <misbahuddinemran2@gmail.com>
2026-08-03 17:00:55 +0600

    Create DatasetDomain.java

---
+++ src/main/java/com/examplatform/modules/evaluation/enums/DatasetDomain.java
@@ -0,0 +1,13 @@
+package com.examplatform.modules.evaluation.enums;
+
+public enum DatasetDomain {
+    HSC_ICT,
+    SSC_ICT,
+    BANK_IT,
+    BCS_ICT,
+    MEDICAL,
+    LAW,
+    BENGALI_QA,
+    ENGLISH_QA,
+    OTHER
+}
