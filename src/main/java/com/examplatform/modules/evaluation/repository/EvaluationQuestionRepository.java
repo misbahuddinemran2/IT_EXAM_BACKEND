94c50c04517fcf50db363f1c485b66d215362df3
misbahuddinemran2 <misbahuddinemran2@gmail.com>
2026-08-04 06:31:20 +0600

    Create EvaluationQuestionRepository.java

---
+++ src/main/java/com/examplatform/modules/evaluation/repository/EvaluationQuestionRepository.java
@@ -0,0 +1,27 @@
+package com.examplatform.modules.evaluation.repository;
+
+import com.examplatform.modules.evaluation.entity.EvaluationQuestion;
+import com.examplatform.modules.evaluation.enums.EvaluationQuestionType;
+import com.examplatform.modules.evaluation.enums.QuestionDifficulty;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface EvaluationQuestionRepository extends JpaRepository<EvaluationQuestion, String> {
+
+    List<EvaluationQuestion> findByDatasetId(String datasetId);
+
+    List<EvaluationQuestion> findByDatasetIdAndIsActiveTrue(String datasetId);
+
+    List<EvaluationQuestion> findByDatasetIdAndDifficulty(String datasetId, QuestionDifficulty difficulty);
+
+    List<EvaluationQuestion> findByDatasetIdAndQuestionType(String datasetId, EvaluationQuestionType questionType);
+
+    long countByDatasetId(String datasetId);
+
+    long countByDatasetIdAndIsActiveTrue(String datasetId);
+
+    @Transactional
+    void deleteByDatasetId(String datasetId);
+}
