d110408fccce9c7e37fb02e08b3adae2d7f8933c
misbahuddinemran2 <misbahuddinemran2@gmail.com>
2026-08-10 11:07:17 +0600

    Create PracticalChapterRepository.java

---
+++ src/main/java/com/examplatform/modules/practical/repository/PracticalChapterRepository.java
@@ -0,0 +1,10 @@
+package com.examplatform.modules.practical.repository;
+
+import com.examplatform.modules.practical.entity.PracticalChapter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PracticalChapterRepository extends JpaRepository<PracticalChapter, String> {
+    List<PracticalChapter> findAllByOrderByOrderNumberAsc();
+}
