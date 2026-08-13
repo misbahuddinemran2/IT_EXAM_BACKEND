e3cb65f97174ab13e38b3d7154eae8f35c27f200
misbahuddinemran2 <misbahuddinemran2@gmail.com>
2026-08-13 10:46:10 +0600

    Create GuidePracticeCqAdminRequest.java

---
+++ src/main/java/com/examplatform/modules/guide/dto/GuidePracticeCqAdminRequest.java
@@ -0,0 +1,38 @@
+package com.examplatform.modules.guide.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GuidePracticeCqAdminRequest {
+    private String topicId;
+    private String stimulus;
+    private String stimulusBn;
+    private boolean isBoardQuestion;
+    private String board;
+    private Integer examYear;
+
+    private String partAQuestion;
+    private String partAModelAnswer;
+    private String partAMarkingScheme;
+    private Integer partAMaxMark;
+
+    private String partBQuestion;
+    private String partBModelAnswer;
+    private String partBMarkingScheme;
+    private Integer partBMaxMark;
+
+    private String partCQuestion;
+    private String partCModelAnswer;
+    private String partCMarkingScheme;
+    private Integer partCMaxMark;
+
+    private String partDQuestion;
+    private String partDModelAnswer;
+    private String partDMarkingScheme;
+    private Integer partDMaxMark;
+
+    private Integer totalMaxMark;
+    private int sortOrder;
+}
