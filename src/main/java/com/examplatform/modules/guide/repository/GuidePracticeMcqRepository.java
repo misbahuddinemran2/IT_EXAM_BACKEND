d0aadc12ed9aca59e408bf72fe3de7690ad42b6d
misbahuddinemran2 <misbahuddinemran2@gmail.com>
2026-08-13 10:41:25 +0600

    Create GuidePracticeMcqRepository.java

---
+++ src/main/java/com/examplatform/modules/guide/repository/GuidePracticeMcqRepository.java
@@ -0,0 +1,15 @@
+package com.examplatform.modules.guide.repository;
+
+import com.examplatform.modules.guide.entity.GuidePracticeMcq;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface GuidePracticeMcqRepository extends JpaRepository<GuidePracticeMcq, String> {
+
+    List<GuidePracticeMcq> findByTopicIdOrderBySortOrderAsc(String topicId);
+
+    List<GuidePracticeMcq> findByTopicIdAndIsBoardQuestionTrueOrderBySortOrderAsc(String topicId);
+
+    List<GuidePracticeMcq> findAllByOrderBySortOrderAsc();
+}
