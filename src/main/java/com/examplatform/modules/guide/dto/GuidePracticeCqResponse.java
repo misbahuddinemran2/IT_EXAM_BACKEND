c5b1df2513efbf02147edaa9cb5a69cbda960415
misbahuddinemran2 <misbahuddinemran2@gmail.com>
2026-08-13 10:45:19 +0600

    Create GuidePracticeCqResponse.java

---
+++ src/main/java/com/examplatform/modules/guide/dto/GuidePracticeCqResponse.java
@@ -0,0 +1,39 @@
+package com.examplatform.modules.guide.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class GuidePracticeCqResponse {
+    private String id;
+    private String topicId;
+    private String stimulus;
+    private String stimulusBn;
+    private boolean isBoardQuestion;
+    private String board;
+    private Integer examYear;
+
+    private String partAQuestion;
+    private String partAModelAnswer;
+    private String partAMarkingScheme;
+    private Integer partAMaxMark;
+
+    private String partBQuestion;
+    private String partBModelAnswer;
+    private String partBMarkingScheme;
+    private Integer partBMaxMark;
+
+    private String partCQuestion;
+    private String partCModelAnswer;
+    private String partCMarkingScheme;
+    private Integer partCMaxMark;
+
+    private String partDQuestion;
+    private String partDModelAnswer;
+    private String partDMarkingScheme;
+    private Integer partDMaxMark;
+
+    private Integer totalMaxMark;
+    private int sortOrder;
+}
