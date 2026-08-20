8f42a8da426d5bd417f68173ff6491fb0f652ad0
misbahuddinemran2 <misbahuddinemran2@gmail.com>
2026-08-21 00:52:57 +0600

    Update QuestionStatsResponse.java

--- src/main/java/com/examplatform/modules/liveexam/dto/QuestionStatsResponse.java
+++ src/main/java/com/examplatform/modules/liveexam/dto/QuestionStatsResponse.java
@@ -9,9 +9,10 @@ import lombok.*;
 @Builder
 public class QuestionStatsResponse {
     private String questionId;
+    private String questionText;
     private long totalAttempts;
     private long totalCorrect;
     private long totalWrong;
     private long totalSkipped;
-    private double accuracyRate; // percentage
+    private double accuracyRate;
 }
